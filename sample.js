8fb0cb3d714cb1210d93aaf161c5dc3d0b00205c
Hasan Shahid <hasshahi@publicisgroupe.net>
2019-10-21 16:40:08 +0530

    Variables

--- sample.js
+++ sample.js
@@ -1,2 +1,21 @@
 //Hello World
 console.log("Hello World");
+
+console.log("\n");
+
+// Variables
+let message; // Variable declaration
+message = "Hello"; // Variable Definition
+console.log(message);
+
+let message2 = "Hello"; // Variable declaration and definition
+console.log(message2);
+
+console.log("\n");
+
+let message3;
+message3 = "Hello";
+message3 = "World";
+console.log(message3);
+
+console.log("\n");
