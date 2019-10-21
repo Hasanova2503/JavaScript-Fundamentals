d3b71f907d1b8c01153a00f18fddfff4d85a9891
Hasan Shahid <hasshahi@publicisgroupe.net>
2019-10-21 16:55:56 +0530

    Different data types

--- sample.js
+++ sample.js
@@ -3,7 +3,7 @@ console.log("Hello World");
 
 console.log("\n");
 
-// Variables
+// Variables --> Named storage for data
 let message; // Variable declaration
 message = "Hello"; // Variable Definition
 console.log(message);
@@ -19,3 +19,27 @@ message3 = "World";
 console.log(message3);
 
 console.log("\n");
+
+//Data Types
+let aString = "Hello World";
+let aNumber = 123456;
+let aBoolean = true;
+let user = {
+  name: "Hasan",
+  age: 22
+}
+let arr = [1, 2, 3, 4];
+let nullValue = null;
+let undefValue;
+let id = Symbol("Id");
+
+console.log(typeof(aString));
+console.log(typeof(aNumber));
+console.log(typeof(aBoolean));
+console.log(typeof(user));
+console.log(typeof(arr));
+console.log(typeof(nullValue));
+console.log(typeof(undefValue));
+console.log(typeof(id));
+console.log(typeof(alert));
+console.log(typeof(Date));
