6c0c47dbd57ad2e69b9a776f5198d150af828648
Hasan Shahid <hasshahi@publicisgroupe.net>
2019-10-30 12:48:44 +0530

    DOM examination

--- sample.js
+++ sample.js
@@ -334,14 +334,14 @@ console.log("\u{1F60D}");
 console.log("\n");
 
 //Template literals
-let html
-html = `
-  <ul>
-    <li>FName: ${fName}</li>
-    <li>lName: ${lName}</li>
-  </ul>
-`;
-document.body.innerHTML = html;
+// let html;
+// html = `
+//   <ul>
+//     <li>FName: ${fName}</li>
+//     <li>lName: ${lName}</li>
+//   </ul>
+// `;
+// document.body.innerHTML = html;
 
 //Arrays
 const nums = [1, 5, 3, 6, 7];
@@ -761,3 +761,43 @@ function helloThis() {
   console.log(this);
 }
 helloThis(); // undefined using use strict
+
+console.log("\n");
+
+// DOM examination
+val = document;
+val = document.all;
+val = document.all[1];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList;
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src');
+
+let scripts = document.scripts;
+let scriptArr = Array.from(scripts);
+
+scriptArr.forEach(function(script) {
+  console.log(script);
+});
+
+console.log(val);
